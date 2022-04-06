29e47812fc5d725a8ebd9a948964ec181102f21b
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-06 15:56:34 -0300

    import fix

--- src/server.js
+++ src/server.js
@@ -5,9 +5,10 @@ require('dotenv').config({
 const port = process.env.NODE_PORT;
 
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
