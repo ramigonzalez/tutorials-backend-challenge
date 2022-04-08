b474fb8d890e9bacfb65e4232d9ab25bbacd6d57
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-08 20:00:56 -0300

    * Database initialization before server listen * middlewares added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ require('dotenv').config({
     path: process.env.NODE_ENV === "test" ? "./config/test.env" : "./config/dev.env" }
 );
 
+const Repository = require('./repositories');
+const { okHandler, errorHandler, notFound } = require('./middlewares');
+
+
+
+
 const port = process.env.NODE_PORT;
 
 const express = require('express');
@@ -15,7 +21,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(morgan('dev'));
 
-require('./controllers/routes')(app);
+require('./routes')(app);
 
-app.listen(port, () => console.info(`Tutorials Server is listening on port ${port}`));
+app.use(notFound);
+app.use(okHandler);
+app.use(errorHandler);
 
+
+
+Repository.init()
+.then(() => app.listen(port, () => console.info(`Tutorials Server is listening on port ${port}`)))
+.catch((err) => {
+        console.error('Could not connect with database', err.message);
+        process.exit(1);
+    });
