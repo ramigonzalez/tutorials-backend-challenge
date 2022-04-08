8c4325c7cdf5a50962d7043703bad0b47ee4bb47
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-08 19:56:18 -0300

    tutorial model schema definition

---
+++ src/repositories/tutorial.model.js
@@ -0,0 +1,44 @@
+module.exports = (Types, sequelize) => 
+
+    sequelize.define("tutorial", 
+        {
+            id: {
+                type: Types.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true,
+            },
+            title: {
+                type: Types.STRING,
+                allowNull: false
+            },
+            video_url: {
+                type: Types.STRING,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: 'Video URL cannot be null'
+                    },
+                    notEmpty: {
+                        msg: 'Video URL cannot be empty'
+                    },
+                    isUrl: {
+                        msg: 'Invalid Video URL format'
+                    }
+                }
+            },
+            description: {
+                type: Types.STRING
+            },
+            published_status: {
+                type: Types.ENUM('PENDING','IN PROGRESS', 'PUBLISHED'),
+                allowNull: false,
+            }
+
+        },
+        {
+            paranoid: true,
+            timestamps: true,
+        }
+    );
+   
