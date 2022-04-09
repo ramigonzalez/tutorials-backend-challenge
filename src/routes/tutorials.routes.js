4164d0aef0be67520f11b05ab19d32bd2d493091
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-09 16:55:46 -0300

    All tutorial routes created

--- src/routes/tutorials.routes.js
+++ src/routes/tutorials.routes.js
@@ -3,10 +3,41 @@ const { allowAdminOnly, verifyUserToken } = require('../middlewares');
 
 module.exports = app => {
     
-    const tutorialsController = new TutorialsController();
+    const controller = new TutorialsController();
     const router = require('express').Router();
 
-    router.get('/', verifyUserToken, allowAdminOnly, tutorialsController.getTutorials.bind(tutorialsController));
+    router.get('/', 
+        verifyUserToken, 
+        controller.getAllTutorials.bind(controller));
+    
+    router.get('/:id', 
+        verifyUserToken, 
+        controller.getTutorial.bind(controller));
+    
+    router.get('/token', 
+        verifyUserToken, 
+        allowAdminOnly, 
+        controller.getTutorialCreationToken.bind(controller));
+    
+    router.post('/', 
+        verifyUserToken, 
+        allowAdminOnly, 
+        controller.create.bind(controller));
+    
+    router.put('/:id',
+        verifyUserToken, 
+        allowAdminOnly, 
+        controller.update.bind(controller));
+    
+    router.delete('/:id', 
+        verifyUserToken, 
+        allowAdminOnly,
+        controller.delete.bind(controller));
+    
+    router.delete('/mass_delete', 
+        verifyUserToken, 
+        allowAdminOnly, 
+        controller.bulkDelete.bind(controller));
 
     app.use('/v1/api/tutorials', router);
 }
