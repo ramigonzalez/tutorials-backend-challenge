2ad0eb9454d789c4345a082c8d727f6e627df0ec
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-10 14:20:59 -0300

    tutorial service logic implementation

--- src/services/tutorials.service.js
+++ src/services/tutorials.service.js
@@ -1,54 +1,89 @@
 const Repository = require('../repositories');
-const UserService = require('./user.service');
+const Op = require('Sequelize').Op;
+
+const jwt = require('jsonwebtoken');
+const {
+    getPrivateKey,
+    getRandomNumber,
+    tutorialTokenSignOptions,
+} = require('../utils/jwttoken');
 
 module.exports = class TutorialService {
-
-    constructor() {
-        this.userService = new UserService();
+    constructor(repository = Repository) {
+        this.repository = repository;
     }
 
-    async findAll(reqOptions, { email }) {
+    async findAll(reqOptions) {
         try {
-            const user = await this.userService.getLoggedUser(email);
-            const queryOptions = this.buildQueryOptions(reqOptions, user);
-            return await Repository.Tutorial.findAll(queryOptions);
+            const queryOptions = this.buildQueryOptions(reqOptions);
+            return await this.repository.Tutorial.findAll(queryOptions);
         } catch (err) {
-            console.error(`[TutorialService.findAll] Error while retrieving tutorials ${err.message}`);
+            console.error(
+                `[TutorialService.findAll] Error while retrieving tutorials ${err.message}`
+            );
             throw err;
             // throw new BadRequestException("", err);
         }
     }
 
-    buildQueryOptions({ filters, sorting, pagination }, { id }) {
-        const reqOptions = {} ;
+    buildQueryOptions({ filters, sorting, pagination }) {
+        const reqOptions = {};
 
         // Add not null conditions.
-        if (filters) reqOptions.filters = filters; 
-        if (sorting) reqOptions.sorting = sorting; 
-        if (pagination) reqOptions.pagination = pagination; 
+        if (filters) reqOptions.filters = filters;
+        if (sorting) reqOptions.sorting = sorting;
+        if (pagination) reqOptions.pagination = pagination;
 
         console.log('reqOptions', reqOptions);
 
         let queryOptions = {};
 
-        queryOptions = this.prepareWhereCondition(queryOptions, reqOptions, id);
+        queryOptions = this.prepareWhereCondition(queryOptions, reqOptions);
         queryOptions = this.prepareOrderByCondition(queryOptions, reqOptions);
         queryOptions = this.preparePagination(queryOptions, reqOptions);
+        queryOptions = this.prepareIncludeCondition(queryOptions);
+        queryOptions.attributes = { exclude: ['deletedAt'] };
 
         return queryOptions;
     }
 
-    prepareWhereCondition(opt = {}, { filters }, userId) {
+    prepareIncludeCondition(opt = {}) {
+        opt.include = [
+            {
+                model: Repository.User,
+                attributes: ['id', 'first_name', 'last_name', 'email', 'role'],
+            },
+        ];
+        return opt;
+    }
+
+    prepareWhereCondition(opt = {}, { filters }) {
+        let condition;
         if (filters) {
-            opt.where = filters;
+            const conditionArray = [];
+
+            if (filters.title)
+                conditionArray.push({ title: { [Op.like]: `%${filters.title}%` } });
+            if (filters.description)
+                conditionArray.push({
+                    description: { [Op.like]: `%${filters.description}%` },
+                });
+
+            if (conditionArray.length !== 0) {
+                if (!filters.condition || filters.condition === 'OR') {
+                    condition = { [Op.or]: conditionArray };
+                } else if (filters.condition === 'AND') {
+                    condition = { [Op.and]: conditionArray };
+                }
+            }
         }
-        opt.where.userId = userId;
+        if (condition) opt.where = condition;
         return opt;
     }
 
     prepareOrderByCondition(opt = {}, { sorting }) {
         const order = [];
-        if (sorting) { 
+        if (sorting) {
             order.push(Object.entries(sorting).map(([_, value]) => value));
             opt.order = order;
         }
@@ -56,10 +91,80 @@ module.exports = class TutorialService {
     }
 
     preparePagination(opt = {}, { pagination }) {
-        if (pagination) { 
+        if (pagination) {
             opt.limit = pagination.limit;
             opt.offset = pagination.offset;
         }
         return opt;
     }
-}
+
+    async find(id) {
+        try {
+            const ret = await Repository.Tutorial.findByPk(
+                id,
+                this.prepareIncludeCondition()
+            );
+            if (!ret) throw new Error(`Tutorial with id: ${id} not found`); //NotFoundException
+            return ret.get();
+        } catch (err) {
+            console.error(
+                `[TutorialService.find] Error while retrieving tutorial with id: ${id}`,
+                err.message
+            );
+            throw err;
+            // throw new BadRequestException("", err);
+        }
+    }
+
+    getTutorialCreationToken(timestamp) {
+        const privateKey = getPrivateKey();
+        const createdAt = new Date(timestamp).toISOString();
+        const payload = {
+            timestamp,
+            createdAt,
+            randomness: getRandomNumber(),
+        };
+        return jwt.sign(payload, privateKey, tutorialTokenSignOptions());
+    }
+
+    async create(tutorial, userId) {
+        const ret = await Repository.Tutorial.create({
+            ...tutorial,
+            userId,
+            video_url: tutorial.videoUrl,
+            published_status: 'PENDING',
+        });
+        return ret.get();
+    }
+
+    async update(tutorial, id) {
+        const ret = await Repository.Tutorial.update(tutorial, { where: { id } });
+        return ret.get();
+    }
+
+    async delete(id) {
+        try {
+            const count = await Repository.Tutorial.destroy({ where: { id } });
+            if (count === 1) return `Tutorial with id: ${id} was deleted successfully`;
+            else return `No deletion occurred`;
+        } catch (error) {
+            throw new Error(`Could not delete tutorial with id: ${id}`, error.message);
+        }
+    }
+
+    async deleteAllByUser(userId) {
+        try {
+            const count = await Repository.Tutorial.destroy({
+                where: { userId },
+                truncate: false,
+            });
+            if (count === 1) return `Tutorial with id: ${id} was deleted successfully`;
+            else return `No deletion occurred`;
+        } catch (error) {
+            throw new Error(
+                `Could not delete tutorials of logged user: ${userId}`,
+                error.message
+            );
+        }
+    }
+};
