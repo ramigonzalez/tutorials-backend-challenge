fd98b5b7e378c0a935d5e61182c5b28551c9f5aa
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-11 01:13:58 -0300

    server env var configuration

---
+++ config/environment.js
@@ -0,0 +1,26 @@
+module.exports.isDevelopmentOrTesting = () => {
+    const env = process.env.NODE_ENV;
+    console.log(' ** env **', env);
+    return env === 'development' || env === 'test';
+};
+
+module.exports.loadConfiguration = () => {
+    require('dotenv').config({
+        path: resolvePath(),
+    });
+};
+
+const resolvePath = () => {
+    let path;
+    switch (process.env.NODE_ENV) {
+        case 'test':
+            path = './config/test.env';
+            break;
+        case 'production':
+            path = './config/prod.env';
+            break;
+        default:
+            path = './config/dev.env';
+    }
+    return path;
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
-require('dotenv').config({
-    path: process.env.NODE_ENV === 'test' ? './config/test.env' : './config/dev.env',
-});
+const { loadConfiguration } = require('../config/environment');
+loadConfiguration();
 
 const Repository = require('./repositories');
 const { okHandler, errorHandler, notFound } = require('./middlewares');
@@ -8,6 +7,7 @@ const routes = require('./routes');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const { BaseError } = require('./exceptions');
 
 const app = express();
 const port = process.env.NODE_PORT;
@@ -17,18 +17,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
-routes(app);
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Error: unhandledRejection');
+    throw reason;
+});
 
+process.on('uncaughtException', (error) => {
+    console.error('Error: uncaughtException');
+    if (!error instanceof BaseError) {
+        process.exit(1);
+    }
+});
+
+routes(app);
 app.use(notFound);
 app.use(okHandler);
 app.use(errorHandler);
 
 Repository.init()
-    .then(() =>
-        app.listen(port, () =>
-            console.info(`Tutorials Server is listening on port ${port}`)
-        )
-    )
+    .then(() => app.listen(port, () => console.info(`Tutorials Server is listening on port ${port}`)))
     .catch((err) => {
         console.error('Could not connect with database', err.message);
         process.exit(1);
