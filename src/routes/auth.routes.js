62adff97d0f4f515df90616a189d07e26f0c9dcf
Ramiro Gonzalez <ramirogc21@gmail.com>
2022-04-11 01:06:46 -0300

    method not allowed control implemented

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 const AuthController = require('../controllers/auth.controller');
+const { allowedMethods } = require('../middlewares/method-not-allowed');
+const httpMethods = require('./http-methods');
 
 module.exports = (app) => {
     const controller = new AuthController();
     const router = require('express').Router();
 
-    router.post('/', controller.authenticate.bind(controller));
+    router
+        .post('/', controller.authenticate.bind(controller))
+        .use('/', (req, _, next) => allowedMethods(req, _, next, [httpMethods.POST]));
 
     app.use('/v1/api/auth', router);
 };
