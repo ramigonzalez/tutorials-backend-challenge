3c99acbbbc8d77876d7af22008a75f9325716df9
Ramiro Gonzalez <40214297+ramigonzalez@users.noreply.github.com>
2022-04-12 10:50:26 -0300

    Validation middlewares (#3)
    * Validation middleware implementation
    * middlewares created
    * regex added to tutorial router to resolve express miss handling
    
    * controller validation removed

--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -1,13 +1,13 @@
 const AuthController = require('../../controllers/auth.controller');
-const { allowedMethods } = require('../../middlewares/method-not-allowed');
+const { verifyUserToken, allowAdminOnly, allowedMethods, validateErrors, rules } = require('../../middlewares');
 const httpMethods = require('../http-methods');
 
 module.exports = (app) => {
-    const controller = new AuthController();
-    const router = require('express').Router();
+    const authController = new AuthController();
 
+    const router = require('express').Router();
     router
-        .post('/', controller.authenticate.bind(controller))
+        .post('/', rules.auth.authentication(), validateErrors, authController.authenticate.bind(authController))
         .use('/', (req, res, next) => allowedMethods(req, res, next, [httpMethods.POST]));
 
     app.use('/api/v1/auth', router);
--- src/routes/v1/tutorials.routes.js
+++ src/routes/v1/tutorials.routes.js
@@ -5,48 +5,66 @@ const {
     verifyUserToken,
     verifyTutorialToken,
     allowedMethods,
+    validateErrors,
+    rules,
 } = require('../../middlewares');
-
 const httpMethods = require('../http-methods');
 
 module.exports = (app) => {
     const controller = new TutorialsController();
     const router = require('express').Router();
 
-    router
-        .post(
-            path.TUTORIALS,
-            verifyUserToken,
-            allowAdminOnly,
-            verifyTutorialToken,
-            tutorialTokenExpiration,
-            controller.create.bind(controller)
-        )
-        .get(path.TUTORIALS, verifyUserToken, controller.getAllTutorials.bind(controller))
-        .use(path.TUTORIALS, (req, res, next) => allowedMethods(req, res, next, [httpMethods.POST, httpMethods.GET]));
+    router.use(path.TUTORIAL_TOKEN, (req, res, next) => allowedMethods(req, res, next, [httpMethods.GET]));
+    router.get(
+        path.TUTORIAL_TOKEN,
+        verifyUserToken,
+        allowAdminOnly,
+        controller.getTutorialCreationToken.bind(controller)
+    );
 
-    router
-        .get(path.TUTORIAL_TOKEN, verifyUserToken, allowAdminOnly, controller.getTutorialCreationToken.bind(controller))
-        .use(path.TUTORIAL_TOKEN, (req, res, next) => allowedMethods(req, res, next, [httpMethods.GET]));
+    router.use(path.SINGLE_TUTORIAL, (req, res, next) =>
+        allowedMethods(req, res, next, [httpMethods.GET, httpMethods.PUT, httpMethods.DELETE])
+    );
+    router.get(path.SINGLE_TUTORIAL, verifyUserToken, controller.getTutorial.bind(controller));
+    router.put(
+        path.SINGLE_TUTORIAL,
+        rules.tutorials.update(),
+        validateErrors,
+        verifyUserToken,
+        allowAdminOnly,
+        controller.update.bind(controller)
+    );
+    router.delete(path.SINGLE_TUTORIAL, verifyUserToken, allowAdminOnly, controller.delete.bind(controller));
 
-    router
-        .get(path.SINGLE_TUTORIAL, verifyUserToken, controller.getTutorial.bind(controller))
-        .put(path.SINGLE_TUTORIAL, verifyUserToken, allowAdminOnly, controller.update.bind(controller))
-        .delete(path.SINGLE_TUTORIAL, verifyUserToken, allowAdminOnly, controller.delete.bind(controller))
-        .use(path.SINGLE_TUTORIAL, (req, res, next) =>
-            allowedMethods(req, res, next, [httpMethods.GET, httpMethods.PUT, httpMethods.DELETE])
-        );
+    router.use(path.MASS_DELETE, (req, res, next) => allowedMethods(req, res, next, [httpMethods.DELETE]));
+    router.delete(path.MASS_DELETE, verifyUserToken, allowAdminOnly, controller.bulkDelete.bind(controller));
 
-    router
-        .delete(path.MASS_DELETE, verifyUserToken, allowAdminOnly, controller.bulkDelete.bind(controller))
-        .use(path.MASS_DELETE, (req, res, next) => allowedMethods(req, res, next, [httpMethods.DELETE]));
+    router.use(path.TUTORIALS, (req, res, next) => allowedMethods(req, res, next, [httpMethods.POST, httpMethods.GET]));
+    router.post(
+        path.TUTORIALS,
+        verifyUserToken,
+        allowAdminOnly,
+        verifyTutorialToken,
+        tutorialTokenExpiration,
+        rules.tutorials.create(),
+        validateErrors,
+        controller.create.bind(controller)
+    );
+    router.get(
+        path.TUTORIALS,
+        verifyUserToken,
+        rules.tutorials.getAll(),
+        validateErrors,
+        controller.getAllTutorials.bind(controller)
+    );
 
-    app.use('/api/v1/tutorials', router);
+    app.use('/api/v1', router);
 };
 
 const path = {
-    TUTORIALS: '/',
-    TUTORIAL_TOKEN: '/token',
-    SINGLE_TUTORIAL: '/:id([\\d]+)',
-    MASS_DELETE: '/mass_delete',
+    TUTORIALS: /\/tutorials((\?\w+\=\w*)(&\w+\=\w*)*)*$/,
+    
+    TUTORIAL_TOKEN: '/tutorials/token',
+    SINGLE_TUTORIAL: /\/tutorials\/\d+$/,
+    MASS_DELETE: '/tutorials/mass_delete',
 };
